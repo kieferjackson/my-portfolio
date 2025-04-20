cc320d59f90b585d2d3479f580706bbbb801a422
Kiefer Jackson <kieferleejackson@gmail.com>
2025-04-20 15:37:56 -0700

    Impl dynamic project data for pages

--- my-next-portfolio/src/app/projects/[project]/page.js
+++ my-next-portfolio/src/app/projects/[project]/page.js
@@ -1,12 +1,31 @@
+import Link from "next/link";
+
+import projects from "@/app/data/projects.json";
+
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
-  const projects = ["next"];
- 
-  return projects.map((project) => ({ project }))
+  return Array.from(
+    Object.keys(projects)).map((project) => ({ project })
+  );
 }
 
 export default async function Page({ params }) {
   const { project } = await params;
-  return <div>My Project: {project}</div>
+
+  const projectData = projects[project];
+
+  return (
+    <div>
+      <h1>My Project: {project}</h1>
+      <p>Name: {projectData.name}</p>
+      {(projectData.href ? <Link href={projectData.href}>Webpage Link</Link> : <code>Nothing here</code>)}
+      {(projectData.repo ? <Link href={projectData.repo}>Github Repo</Link> : <code>Nothing here</code>)}
+      <ul>
+        {projectData.tech.map((tech) => {
+          return <li key={tech}>{tech}</li>;
+        })}
+      </ul>
+    </div>
+  )
 }
--- my-next-portfolio/src/app/projects/page.js
+++ my-next-portfolio/src/app/projects/page.js
@@ -1,13 +1,18 @@
+import Link from "next/link";
+
 import styles from "./page.module.css";
+import projects from "@/app/data/projects.json";
 
 export default function Projects() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <ul>
-            <li>Project 1</li>
-            <li>Project 2</li>
-            <li>Project 3</li>
+        {Array.from(Object.keys(projects)).map((project) => (
+          <li key={project}>
+            <Link href={`/projects/${project}`}>{projects[project].name}</Link>
+          </li>
+        ))}
         </ul>
       </main>
     </div>
