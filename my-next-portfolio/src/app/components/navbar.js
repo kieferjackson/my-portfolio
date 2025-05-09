de7613f68624a6c9aacbe25f6e5d07ddd7f62ffa
Kiefer Jackson <kieferleejackson@gmail.com>
2025-05-08 23:06:17 -0700

    Improvements + styles for navbar

--- my-next-portfolio/src/app/components/navbar.js
+++ my-next-portfolio/src/app/components/navbar.js
@@ -1,19 +1,36 @@
+"use client"
+
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 import styles from "./navbar.module.css";
 
 export default function NavBar() {
-    const menuItems = new Map([
+    const menuItemMap = new Map([
         ["about", "About Me"],
         ["projects", "Projects"]
     ]);
 
+    const pathname = usePathname();
+    const [ basepath ] = pathname.slice(1).split('/');
+
     return (
-      <nav className={styles.navbar}>
-        {Array.from(menuItems.entries()).map(([ key, title ]) => {
-            return <Link key={key} href={`/${key}`} className={styles.link}>{title}</Link>
-        })}
-      </nav>
+        <nav className={styles.navbar} role="navigation">
+            <a href="/">
+                <img src="/svg/kj-logo.svg" width={48} height={48} alt="KJ Logo" />
+            </a>
+            
+            {([ ...menuItemMap.entries() ]).map(([ route, title ], index) => {
+                const attrs = {};
+
+                if (route == basepath) {
+                    attrs["aria-current"] = "page";
+                    attrs["style"] = { boxShadow: "0 -2px 0 var(--amp-yellow) inset" };
+                }
+
+                return <Link key={index} href={`/${route}`} { ...attrs } className={styles["navbar-link"]}>{title}</Link>
+            })}
+        </nav>
     );
   }
 
